033200e2c458138b7f85da6813bf4bd7a5be93ab
olganagorna <olechka.nagorna@gmail.com>
2015-09-02 10:32:03 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $(".select").fadeIn("450");
-    $(".choose_weather").fadeIn("150");
-    $(".choose_region").fadeIn("300");
-    $(".submit_button").fadeIn("450");
+    $(".select").fadeIn(150);
+    $(".choose_weather").fadeIn(300);
+    $(".choose_region").fadeIn(450);
+    $(".submit_button").fadeIn(600);
 });
