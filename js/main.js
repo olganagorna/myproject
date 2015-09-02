e37166d2f4ff25a87e099ecac8995a4f8580cb5e
olganagorna <olechka.nagorna@gmail.com>
2015-09-02 12:23:59 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     setTimeout(function() {
         $("p").removeClass("hidden_with_opacity");
-    }, 1500);
+    }, 200);
     setTimeout(function() {
         $("div").removeClass("hidden_with_opacity");
-    }, 3000);
+    }, 400);
     setTimeout(function() {
         $(".choose_weather").removeClass("hidden_with_opacity");
     }, 4500);
     setTimeout(function() {
         $("button").removeClass("hidden_with_opacity");
-    }, 6000);
+    }, 600);
 
 });
