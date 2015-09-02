8e72ce5bd8e80f1e47e4e66861425031a874ac38
olganagorna <olechka.nagorna@gmail.com>
2015-09-02 11:32:48 +0000

    update

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
     setTimeout(function() {
         $("p").removeClass("hidden_with_opacity");
     }, 1500);
+    setTimeout(function() {
+        $("choose_region").removeClass("hidden_with_opacity");
+    }, 1500);
+    setTimeout(function() {
+        $("choose_weather").removeClass("hidden_with_opacity");
+    }, 1500);
+    setTimeout(function() {
+        $("button").removeClass("hidden_with_opacity");
+    }, 1500);
 
 });
