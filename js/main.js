e99d994acf12fe42776beea381433543ff3a7a11
olganagorna <olechka.nagorna@gmail.com>
2015-09-03 16:44:59 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 $(document).ready(function(){
     setTimeout(function() {
-        $("p").removeClass("hidden_with_opacity");
+        $('.welcome_text').removeClass('hidden_with_opacity');
     }, 200);
     setTimeout(function() {
-        $(".choose_weather").removeClass("hidden_with_opacity");
-    }, 400);
+        $('.choose_weather').removeClass('hidden_with_opacity');
+    }, 500);
     setTimeout(function() {
-        $(".choose_region").removeClass("hidden_with_opacity");
-    }, 600);
-    setTimeout(function() {
-        $("button").removeClass("hidden_with_opacity");
+        $('.choose_region').removeClass('hidden_with_opacity');
     }, 800);
+    setTimeout(function() {
+        $('#search_button').removeClass('hidden_with_opacity');
+    }, 1200);
+    $('#search_button').click(function(event) {
+        event.preventDefault();
+        if ($(this).hasClass('loading')) {
+            return;
+        }
+        $(this).addClass('round');
+        setTimeout(function() {
+            $('#search_button').addClass('loading');
+        }, 500);
+        $('.welcome_text').addClass('fadeOutUp');
+        $('.choose_weather').addClass('fadeOutUp');
+        $('.choose_region').addClass('fadeOutUp');
+        setTimeout(function() {
+        $('#search_button').removeClass('round').removeClass('loading').addClass('slide_out_up');
+        $('.results_table').removeClass('hidden_down');
+        $('.btn_table').removeClass('hidden_down');
 
+        $('#table_wrapper').removeClass('hidden_down').addClass('slide_out_up');
+        }, 1500);
+        
+        
+    });
+    $('.city').click(function(event) {
+        event.preventDefault();
+        $('#search_button').removeClass('slide_out_up').addClass('fadeOutUpBig');
+        $('.results_table').addClass('fadeOutUpBig');
+        $('.btn_table').addClass('fadeOutUpBig');
+        $('#table_wrapper').removeClass('slide_out_up').addClass('fadeOutUpBig');
+        $('#wrapper_weather_table').removeClass('hidden_down').addClass('slide_out_up_big');
+    });
 });
