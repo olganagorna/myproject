f45859682efefc2a0d1cfbc90a20c6ac9c7b053d
olganagorna <olechka.nagorna@gmail.com>
2015-09-04 23:04:20 +0300

    Delete getWeatherByCity.js

--- js/getWeatherByCity.js
+++
@@ -1,9 +0,0 @@
-function getWeatherByCity(lang, fnOK, fnError, city_name) {
-    $.getJSON(
-        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + city_name + '&cnt=5&units=metric' + '&lang=' + lang + '&callback=?',
-        function (data) {
-            fnOK.call(this, data);
-        }
-    );
-}
